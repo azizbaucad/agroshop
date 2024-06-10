bcefc35303863ed03bc2f8e7d79b5db3bff21e46
aaba <aaba>
2024-06-10 23:24:25 +0000

    feat: welcome components agro shop is add

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -31,10 +31,10 @@ export default function Header() {
                   </svg>
                 </div>
                 <input type="search" id="default-search" class="block w-full p-1.5 ps-10 font-medium text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Search ..." required />
-                <button type="submit" class="hidden text-white absolute end-2.5 bottom-2.5 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 me-2 mb-2">Search</button>
+                <button type="submit" class="hidden text-white absolute end-2.5 bottom-2.5 bg-green-400 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-400 dark:focus:ring-green-800 me-2 mb-2">Search</button>
               </div>
             </form>
-            <button type="button" class="transition-all duration-500 text-white  bg-green-700 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-white font-medium text-sm rounded-full px-5 py-2 text-center inline-flex items-center dark:focus:ring-gray-500 me-2 mb-2">
+            <button type="button" class="transition-all duration-500 text-black  bg-green-400 hover:bg-green-300 focus:ring-4 focus:outline-none focus:ring-white font-medium text-sm rounded-full px-5 py-2 text-center inline-flex items-center dark:focus:ring-gray-500 me-2 mb-2">
               Vendre un produit
             </button>
           </div>
@@ -45,7 +45,7 @@ export default function Header() {
             <ul className="flex space-x-2">
               <li className="relative inline-block">
                 <a className="" href="">
-                  <div className="absolute -top-1 right-0 z-10 bg-green-700 text-xs text-white font-bold px-1 py-0.5 rounded-full">12</div>
+                  <div className="absolute -top-1 right-0 z-10 bg-green-400 text-xs text-black font-bold px-1 py-0.5 rounded-full">12</div>
                   <svg className="h-9 lg:h-10 p-2 text-gray-500 svg-inline fa fa-shopping-cart fa-w-18 fa-9x" aria-hidden="true" focusable="false" data-prefix="far" data-icon="shopping-cart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                     <path fill="currentColor" d="M551.991 64H144.28l-8.726-44.608C133.35 8.128 123.478 0 112 0H12C5.373 0 0 5.373 0 12v24c0 6.627 5.373 12 12 12h80.24l69.594 355.701C150.796 415.201 144 430.802 144 448c0 35.346 28.654 64 64 64s64-28.654 64-64a63.681 63.681 0 0 0-8.583-32h145.167a63.681 63.681 0 0 0-8.583 32c0 35.346 28.654 64 64 64 35.346 0 64-28.654 64-64 0-18.136-7.556-34.496-19.676-46.142l1.035-4.757c3.254-14.96-8.142-29.101-23.452-29.101H203.76l-9.39-48h312.405c11.29 0 21.054-7.869 23.452-18.902l45.216-208C578.695 78.139 567.299 64 551.991 64zM208 472c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm256 0c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm23.438-200H184.98l-31.31-160h368.548l-34.78 160z"></path>
                   </svg>
@@ -54,7 +54,7 @@ export default function Header() {
 
               <li className="relative inline-block">
                 <a className="" href="">
-                  <div className="absolute -top-1 right-0 z-10 bg-green-700 text-white text-xs font-bold px-1 py-0.5 rounded-full">30</div>
+                  <div className="absolute -top-1 right-0 z-10 bg-green-400 text- black text-xs font-bold px-1 py-0.5 rounded-full">30</div>
                   <svg className="h-9 lg:h-10 p-2 text-gray-500 svg-inline fa fa-heart fa-w-16 fa-9x" aria-hidden="true" focusable="false" data-prefix="far" data-icon="heart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                     <path fill="currentColor" d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"></path>
                   </svg>
--- app/components/PerformanceChart.jsx
+++ app/components/PerformanceChart.jsx
@@ -9,20 +9,21 @@ import welcome from '@/src/images/welcome.png';
 import "swiper/css";
 import "swiper/css/pagination";
 
-
-
 export default function PerformanceChart() {
 
   return (
-    <div className="flex justify-center md:w-full bg-gray-200 items-start mt-20">
-              <Image
-               src={welcome}
-               alt="Title One Plus"
-               width={450}
-               height={450}
-               quality={100}
-               className="w-[100%] h-[75%] object-cover"
-              />
+    <>
+    
+    <div className="flex flex-col mt-20 bg-green-400 w-[100%] h-[35%] bg-cover blur-0 items-left justify-center">
+      <div className="justify-center items-center ml-10 space-y-4">
+      <p className="font-bold text-6xl text-black ml-60 mb-2">Agro Shops.</p>
+      <span className="font-medium text-xl text-black items-start justify-start ml-60">The First Online Marketplace for Agro Business</span>
+      </div>
     </div>
+
+    {/* <div className="flex flex-col mt-20 bg-ttuPattern w-[45%] h-[100%] bg-cover blur-0 items-center justify-center">
+    </div> */}
+    
+    </>
   );
 }
